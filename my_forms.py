ce5d650fbe063a0a2bc113236933ca7b6171ac71
repl.it user <replituser@example.com>
2023-05-13 18:47:06 +0300

    new_blog_3

--- my_forms.py
+++ my_forms.py
@@ -2,30 +2,31 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
+import random
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+
+class CommentForm(FlaskForm):
+    submit = SubmitField("Submit Comment")
+    comment_text = CKEditorField("Comment", validators=[DataRequired()])
 
 
 ##WTForm
 class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    title = StringField("Blog Post Title", validators=[DataRequired()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
-
-
-class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
-    submit = SubmitField("Sign Me Up!")
 
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Let Me In!")
-
-
-class CommentForm(FlaskForm):
-    comment_text = CKEditorField("Comment", validators=[DataRequired()])
-    submit = SubmitField("Submit Comment")
